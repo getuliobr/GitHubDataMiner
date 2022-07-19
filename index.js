c70efaa11fa29fab18efa45b11a074ca670bb6c0
getuliobr <getulioregis@alunos.utfpr.edu.br>
2022-07-19 13:39:53 -0300

    fetch patch and pull request data

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ import { MongoHelper } from './db/db.js'
 const sleep = ms => new Promise(r => setTimeout(r, ms));
 
 dotenv.config();
-const { MONGOURI, REPO_OWNER, REPO_NAME } = process.env;
+const { MONGO_URI, REPO_OWNER, REPO_NAME, FETCH_PATCH } = process.env;
 
 try {
-  await MongoHelper.connect(MONGOURI);
+  await MongoHelper.connect(MONGO_URI);
 } catch (error) {
   console.error('Couldnt connect to db:', error);
 }
@@ -18,6 +18,8 @@ const issuesCollection = MongoHelper.getCollection('issues');
 const commentsCollection = MongoHelper.getCollection('comments');
 const eventsCollection = MongoHelper.getCollection('events');
 const timelineCollection = MongoHelper.getCollection('timeline');
+const patchCollection = MongoHelper.getCollection('patch');
+const pullCollection = MongoHelper.getCollection('pullrequest');
 
 const { data } = await api.get(`/repos/${REPO_OWNER}/${REPO_NAME}/issues`, {
   'sort': 'created',
@@ -27,15 +29,17 @@ const { data } = await api.get(`/repos/${REPO_OWNER}/${REPO_NAME}/issues`, {
 const { number: issueCount } = data[0];
 
 // TODO: calculate rate limit if unauthenticated
-// 1225 (4900 requests) just to make sure it will fit in rate limit
-const rateLimitTimeout = 3600/1225;
+// 5000 actual rate limit, 4900 just to make sure it works
+
+const rateLimitPerRequestTimeout = 3600/4900;
 
 for(let i = 1; i <= issueCount; i++) {
   try {
-    // TODO: fetch diff/patch data
+    console.log(`Fetching issue ${i} from ${REPO_OWNER}/${REPO_NAME}`);
+
     const { data } = await api.get(`/repos/${REPO_OWNER}/${REPO_NAME}/issues/${i}`);
-    const { id: issue_id, comments_url, events_url, timeline_url } = data;
-    await issuesCollection.insertOne(data);
+    const { id: issue_id, comments_url, events_url, timeline_url, pull_request } = data;
+    await issuesCollection.insertOne({ issue_id, 'issue_data': data });
 
     const { data: comments_data } = await api.get(comments_url);
     await commentsCollection.insertOne({ issue_id, comments_data });
@@ -46,9 +50,23 @@ for(let i = 1; i <= issueCount; i++) {
     const { data: timeline_data } = await api.get(timeline_url);
     await timelineCollection.insertOne({ issue_id, timeline_data });
 
-    await sleep(rateLimitTimeout * 1000);
+    await sleep(4 * rateLimitPerRequestTimeout);
+
+    if(!pull_request) continue;
+
+    const { url: pull_url, patch_url } = pull_request;
+
+    const { data: pr_data } = await api.get(pull_url);
+    await pullCollection.insertOne({ issue_id, pr_data });
+
+    if(FETCH_PATCH === 'true') {
+      const { data: patch_data } = await api.get(patch_url);
+      await patchCollection.insertOne({ issue_id, patch_data });
+    }
+
+    await sleep(rateLimitPerRequestTimeout);
   } catch (error) {
-    console.error(error);
+    console.error(`Got an error while fetching issue ${i}, message: ${error.message}`);
   }
 }
 
