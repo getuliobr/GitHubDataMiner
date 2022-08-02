a372881791046ad7b9d5215b7178d68f311d8de7
getuliobr <getulioregis@alunos.utfpr.edu.br>
2022-08-02 13:38:17 -0300

    fix skip

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const { MONGO_URI, REPO_OWNER, REPO_NAME, FETCH_PATCH } = process.env;
 try {
   await MongoHelper.connect(MONGO_URI);
 } catch (error) {
-  console.error('Couldnt connect to db:', error);
+  console.log('Couldnt connect to db:', error);
 }
 
 const issuesCollection = MongoHelper.getCollection('issues');
@@ -37,61 +37,64 @@ const rateLimitPerRequestTimeout = 3600/4900;
 
 for(let i = 1; i <= issueCount; i++) {
   console.log(`Fetching issue ${i} from ${REPO_OWNER}/${REPO_NAME}`);
+  let issue_data, issue_id;
   try {
     await sleep(rateLimitPerRequestTimeout);
     const { data } = await api.get(`/repos/${REPO_OWNER}/${REPO_NAME}/issues/${i}`);
-    const { id: issue_id, comments_url, events_url, timeline_url, pull_request } = data;
-    await issuesCollection.insertOne({ issue_id, 'issue_data': data });
-  
-    try {
-      await sleep(rateLimitPerRequestTimeout);
-      const { data: comments_data } = await api.get(comments_url);
-      await commentsCollection.insertOne({ issue_id, comments_data });
-    } catch (error) {
-      console.error(`Got an error while fetching issue comments ${i}, message: ${error.message}`);
-    }
-
-    try {
-      await sleep(rateLimitPerRequestTimeout);
-      const { data: events_data } = await api.get(events_url);
-      await eventsCollection.insertOne({ issue_id, events_data });
-    } catch (error) {
-      console.error(`Got an error while fetching issue events ${i}, message: ${error.message}`);
-    }
-    
-    try {
-      await sleep(rateLimitPerRequestTimeout);
-      const { data: timeline_data } = await api.get(timeline_url);
-      await timelineCollection.insertOne({ issue_id, timeline_data });
-    } catch (error) {
-      console.error(`Got an error while fetching issue timeline ${i}, message: ${error.message}`);
-    }
-
-    if(!pull_request) continue;
-
-    const { url: pull_url, patch_url } = pull_request;
-
-    try {
-      await sleep(rateLimitPerRequestTimeout);
-      const { data: pr_data } = await api.get(pull_url);
-      await pullCollection.insertOne({ issue_id, pr_data });
-    } catch (error) {
-      console.error(`Got an error while fetching pull request ${i}, message: ${error.message}`);
-    }
-
-    if(FETCH_PATCH !== 'true') continue;
-
-    try {
-      await sleep(rateLimitPerRequestTimeout);
-      const { data: patch_data } = await api.get(patch_url);
-      await fs.writeFile(`./patch/${issue_id}.patch`, patch_data, { flag: 'w+' });
-    } catch (error) {
-      console.error(`Got an error while fetching pull request ${i} patch, message: ${error.message}`);
-    }
+    issue_data = data;
+    issue_id = data.id;
+    await issuesCollection.insertOne({ issue_id, issue_data });
   } catch (error) {
-    console.error(`Got an error while fetching issue ${i}, message: ${error.message}`);
-    console.error(error);
+    console.log(`Got an error while fetching issue ${i}, message: ${error.message}`);
     continue;
+  }
+
+  const { comments_url, events_url, timeline_url, pull_request } = issue_data;
+
+  try {
+    await sleep(rateLimitPerRequestTimeout);
+    const { data: comments_data } = await api.get(comments_url);
+    await commentsCollection.insertOne({ issue_id, comments_data });
+  } catch (error) {
+    console.log(`Got an error while fetching issue comments ${i}, message: ${error.message}`);
+  }
+
+  try {
+    await sleep(rateLimitPerRequestTimeout);
+    const { data: events_data } = await api.get(events_url);
+    await eventsCollection.insertOne({ issue_id, events_data });
+  } catch (error) {
+    console.log(`Got an error while fetching issue events ${i}, message: ${error.message}`);
+  }
+  
+  try {
+    await sleep(rateLimitPerRequestTimeout);
+    const { data: timeline_data } = await api.get(timeline_url);
+    await timelineCollection.insertOne({ issue_id, timeline_data });
+  } catch (error) {
+    console.log(`Got an error while fetching issue timeline ${i}, message: ${error.message}`);
+  }
+
+  if(!pull_request) continue;
+
+  const { url: pull_url, patch_url } = pull_request;
+
+  try {
+    await sleep(rateLimitPerRequestTimeout);
+    const { data: pr_data } = await api.get(pull_url);
+    await pullCollection.insertOne({ issue_id, pr_data });
+  } catch (error) {
+    console.log(`Got an error while fetching pull request ${i}, message: ${error.message}`);
+  }
+
+  if(FETCH_PATCH !== 'true') continue;
+
+  try {
+    await sleep(rateLimitPerRequestTimeout);
+    const { data: patch_data } = await api.get(patch_url);
+    await fs.writeFile(`./patch/${issue_id}.patch`, patch_data, { flag: 'w+' });
+  } catch (error) {
+    console.log(`Got an error while fetching pull request ${i} patch, message: ${error.message}`);
   }
 }
 
